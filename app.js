9cfbbee985185e436c93a354522328515f318a76
Bianchi-marina <141640079+Bianchi-marina@users.noreply.github.com>
2024-03-09 10:04:47 -0300

    created Api

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+
+const app = express();
+const PORT = 3000;
+
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello Marina!')
-})
+  res.send('Bem-vindo à API');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use((req, res) => {
+  res.status(404).json({ message: 'A solicitação não foi encontrada' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});

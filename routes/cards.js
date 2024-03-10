ac9281df506f4396de6d652798caa99e5c95f0d3
Bianchi-marina <141640079+Bianchi-marina@users.noreply.github.com>
2024-03-10 09:24:50 -0300

    fixed lint

--- routes/cards.js
+++ routes/cards.js
@@ -5,16 +5,16 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    fs.readFile(path.join(__dirname, '..', 'data', 'cards.json'), 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ message: 'Erro interno do servidor' });
-            return;
-        }
+  fs.readFile(path.join(__dirname, '..', 'data', 'cards.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Erro interno do servidor' });
+      return;
+    }
 
-        const cards = JSON.parse(data);
-        res.json(cards);
-    });
+    const cards = JSON.parse(data);
+    res.json(cards);
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,38 +5,38 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    fs.readFile(path.join(__dirname, '..', 'data', 'users.json'), 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ message: 'Erro interno do servidor' });
-            return;
-        }
+  fs.readFile(path.join(__dirname, '..', 'data', 'users.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Erro interno do servidor' });
+      return;
+    }
 
-        const users = JSON.parse(data);
-        res.json(users);
-    });
+    const users = JSON.parse(data);
+    res.json(users);
+  });
 });
 
 router.get('/:userId', (req, res) => {
-    const userId = req.params.userId;
+  const { userId } = req.params;
 
-    fs.readFile(path.join(__dirname, '..', 'data', 'users.json'), 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).json({ message: 'Erro interno do servidor' });
-            return;
-        }
+  fs.readFile(path.join(__dirname, '..', 'data', 'users.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Erro interno do servidor' });
+      return;
+    }
 
-        const users = JSON.parse(data);
-        const user = users.find(user => user._id === userId);
+    const users = JSON.parse(data);
+    const user = users.find((user) => user._id === userId);
 
-        if (!user) {
-            res.status(404).json({ message: 'ID do usuário não encontrado' });
-            return;
-        }
+    if (!user) {
+      res.status(404).json({ message: 'ID do usuário não encontrado' });
+      return;
+    }
 
-        res.json(user);
-    });
+    res.json(user);
+  });
 });
 
 module.exports = router;
